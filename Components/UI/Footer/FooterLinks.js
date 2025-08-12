69c9b1f652dea18ad3875a8f92ec72020327e2f4
dhoat30 <dhoat30@gmail.com>
2025-08-12 20:57:36 +1200

    removed auckland intercity footer links

--- Components/UI/Footer/FooterLinks.js
+++ Components/UI/Footer/FooterLinks.js
@@ -1,16 +1,11 @@
 export const services = [
     { label: "Auckland Movers", url: "/movers/auckland" },
-    
-    { label: "Auckland to Hamilton", url: "/intercity-movers/auckland-to-hamilton" },
-    { label: "Auckland to Tauranga", url: "/intercity-movers/auckland-to-tauranga" },
-    { label: "Auckland to Rotorua", url: "/intercity-movers/auckland-to-rotorua" },
-    { label: "Auckland to Whangārei", url: "/intercity-movers/auckland-to-whangarei" },
-    { label: "Auckland to Christchurch", url: "/intercity-movers/auckland-to-Christchurch" },
-    { label: "Auckland to Wellington", url: "/intercity-movers/auckland-to-Wellington" },
-    { label: "Auckland to Hastings", url: "/intercity-movers/auckland-to-Hastings" },
-    { label: "Auckland to Queenstown", url: "/intercity-movers/auckland-to-Queenstown" },
-    { label: "Auckland to Dunedin", url: "/intercity-movers/auckland-to-Dunedin" },
-    { label: "Auckland to Invercargill", url: "/intercity-movers/auckland-to-Invercargill" },
+    { label: "Christchurch Movers", url: "/movers/christchurch" },
+
+    { label: "Northland Movers", url: "/movers/northland" },
+    { label: "Palmerston North Movers", url: "/movers/palmerston-north" },
+    { label: "Wellington Movers", url: "/movers/wellington" },
+    { label: "Nelson Movers", url: "/movers/nelson" },
 
 
 ]
